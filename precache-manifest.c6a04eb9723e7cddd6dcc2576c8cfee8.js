7a3a6a8b9a56c68494fe18ed051356719ddc0ffc
WoeOm <1015996366@qq.com>
2019-09-27 17:56:07 +0800

    Updates

---
+++ precache-manifest.c6a04eb9723e7cddd6dcc2576c8cfee8.js
@@ -0,0 +1,134 @@
+self.__precacheManifest = [
+  {
+    "revision": "e47853a6babeac4f1aefd0d92b396bfd",
+    "url": "/static/media/Rajdhani-Bold.e47853a6.ttf"
+  },
+  {
+    "revision": "0c7f166d0cb3152220ea",
+    "url": "/static/js/0.d7997e43.chunk.js"
+  },
+  {
+    "revision": "11ceb48d1e3e2e62429f",
+    "url": "/static/js/1.f1b37cbc.chunk.js"
+  },
+  {
+    "revision": "433a4ca91eb3335fc9ac4cddcd49a22e",
+    "url": "/static/media/partner-18.433a4ca9.png"
+  },
+  {
+    "revision": "25a0dd6ee7b636b1ee8e",
+    "url": "/static/js/main.50d808ac.chunk.js"
+  },
+  {
+    "revision": "fac4492e607cae3c3fe3",
+    "url": "/static/js/runtime~main.b17be1fd.js"
+  },
+  {
+    "revision": "df44aea5e7638bb2a087",
+    "url": "/static/js/4.893dcd5a.chunk.js"
+  },
+  {
+    "revision": "f602ba901f9a915aa1fc3906a3b594df",
+    "url": "/static/media/partner-2.f602ba90.png"
+  },
+  {
+    "revision": "89e1d0e45752945feb31",
+    "url": "/static/js/5.21604683.chunk.js"
+  },
+  {
+    "revision": "adb1fc6cfa87ada15fc4",
+    "url": "/static/js/6.940e3ccc.chunk.js"
+  },
+  {
+    "revision": "261245d4ce61960bc0814fd8d2e560cb",
+    "url": "/static/media/roadmap-m.261245d4.png"
+  },
+  {
+    "revision": "274dac43299edb5df55e",
+    "url": "/static/js/7.facb24af.chunk.js"
+  },
+  {
+    "revision": "50818b00053ec46bdfe58dbd2c04bf22",
+    "url": "/static/media/roadmap.50818b00.png"
+  },
+  {
+    "revision": "525fd8554dc9eedf3af6",
+    "url": "/static/js/8.a48475d1.chunk.js"
+  },
+  {
+    "revision": "6c3bd29a179ad44228e6",
+    "url": "/static/js/9.0e7af7ae.chunk.js"
+  },
+  {
+    "revision": "199e3623690bf2235928af1c7cc69514",
+    "url": "/static/media/Rajdhani-SemiBold.199e3623.ttf"
+  },
+  {
+    "revision": "90502cd25752377be23607ae8b112eda",
+    "url": "/static/media/application-1.90502cd2.png"
+  },
+  {
+    "revision": "123dc5d84fb45b769028e37a37a8ced6",
+    "url": "/static/media/news-1.123dc5d8.jpg"
+  },
+  {
+    "revision": "d0aeb0bb8d2661056e509f28ee7378b5",
+    "url": "/static/media/news-2.d0aeb0bb.jpg"
+  },
+  {
+    "revision": "4f0f42e9ae99e1c33404e02994a600f5",
+    "url": "/static/media/lng.4f0f42e9.svg"
+  },
+  {
+    "revision": "8e5534e9c5265802cde98e32cc3244a3",
+    "url": "/static/media/github.8e5534e9.svg"
+  },
+  {
+    "revision": "54c01a1470336fdc2a677083bbc8e6eb",
+    "url": "/static/media/wx.54c01a14.jpg"
+  },
+  {
+    "revision": "f8c7905ecc52e0eabd3376d1edfe29cc",
+    "url": "/static/media/architecture-bg.f8c7905e.svg"
+  },
+  {
+    "revision": "3620e1355dfed3aa42fc4028587e021d",
+    "url": "/static/media/dev-team.3620e135.png"
+  },
+  {
+    "revision": "baa38569239197e77d274afbab7620f2",
+    "url": "/static/media/banner.baa38569.png"
+  },
+  {
+    "revision": "389ccdfdfc922656029e7ea6d99b4702",
+    "url": "/static/media/architecture.389ccdfd.png"
+  },
+  {
+    "revision": "d5ff79d0b28c7596a766af2e0a8e59c2",
+    "url": "/static/media/architecture-en.d5ff79d0.png"
+  },
+  {
+    "revision": "25a0dd6ee7b636b1ee8e",
+    "url": "/static/css/main.28e19bec.chunk.css"
+  },
+  {
+    "revision": "525fd8554dc9eedf3af6",
+    "url": "/static/css/8.d9724f0d.chunk.css"
+  },
+  {
+    "revision": "274dac43299edb5df55e",
+    "url": "/static/css/7.f6433134.chunk.css"
+  },
+  {
+    "revision": "89e1d0e45752945feb31",
+    "url": "/static/css/5.2d118573.chunk.css"
+  },
+  {
+    "revision": "11ceb48d1e3e2e62429f",
+    "url": "/static/css/1.bffe4c77.chunk.css"
+  },
+  {
+    "revision": "a66c06c602d3d7dadb3a872d0246909f",
+    "url": "/index.html"
+  }
+];
