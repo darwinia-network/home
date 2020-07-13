c5f00082cfc43de66194da37ec3d78884262ce80
Hongtao <acarlhong@gmail.com>
2020-07-13 14:37:06 +0800

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.dba9706bb891ba2feb551957ca4e91dc.js"
+  "/precache-manifest.ac1a9a90bf423ea81544a4266508f9fb.js"
 );
 
 workbox.clientsClaim();
