f97ef6fbd2f0ad8925ff32e3e95141fed32c7d8f
JayJay1024 <lygm1062@gmail.com>
2021-04-01 09:38:02 +0800

    Fixed subscribe message

--- src/page/Home/index.js
+++ src/page/Home/index.js
@@ -304,7 +304,7 @@ class Home extends Component {
     subscribe = (text) => {
         axios.post('https://api.darwinia.network/api/subscribe?email=' + text)
             .then((response) => {
-                if (response.data.code === 0) {
+                if (response.status === 200) {
                     this.setState({
                         info: this.props.t('home_page:subscribe_tip')
                     })
