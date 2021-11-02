dff64cd40619035f5f3e4f9fe9c3cda5a938b9c3
JayJay1024 <lygm1062@gmail.com>
2021-11-02 12:07:55 +0800

    fix: mobile heading

--- src/components/PloBanner/PloBanner.js
+++ src/components/PloBanner/PloBanner.js
@@ -1,19 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from './styles.module.scss';
 import classNames from 'classnames/bind';
 import { Container } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
+import { Modal } from 'antd';
 
 import icon01 from './img/icon-01.png';
+import mobileIcon from './img/mobile-icon.png';
+import closeIcon from './img/close-icon.png';
 
 const cx = classNames.bind(styles);
 
-const PloBanner = () => {
+const MobileModal = () => {
+  const [visible, setVisible] = useState(true);
+
+  const handleClickClose = () => {
+    setVisible(false);
+  }
+
   return (
-    // <div className={cx('main')}>Plo Banner</div>
-    <Container>
-      <div className={cx('main')}>
-        <img alt='...' src={icon01} className={cx('banner-logo')} />
+    <Modal
+      visible={visible}
+      footer={null}
+      closeIcon={
+        <img alt='...' src={closeIcon} style={{ width: '40px' }} />
+      }
+      onCancel={handleClickClose}
+    >
+      <div className={cx('mobile-main')}>
+        <img alt='...' src={mobileIcon} className={cx('banner-logo')} />
         <div className={cx('banner-content')}>
           <h5 className={cx('title')}><span role='img' aria-label='fire'>🔥</span> We are launching our Parachain Crowdloan on Polkadot.</h5>
           <p className={cx('desc')}>Contribute your DOT to help build a Web3.0 Bridge Hub by joining our crowdloan, unlock RING and KTON bonus, also BTC and Metaverse limited edition commemorative NFT waiting for you.</p>
@@ -22,7 +37,27 @@ const PloBanner = () => {
           <span>Join our crowdloan</span>
         </Link>
       </div>
-    </Container>
+    </Modal>
+  );
+};
+
+const PloBanner = () => {
+  return (
+    <>
+      <MobileModal className={cx('mobile-modal')} />
+      <Container>
+        <div className={cx('main')}>
+          <img alt='...' src={icon01} className={cx('banner-logo')} />
+          <div className={cx('banner-content')}>
+            <h5 className={cx('title')}><span role='img' aria-label='fire'>🔥</span> We are launching our Parachain Crowdloan on Polkadot.</h5>
+            <p className={cx('desc')}>Contribute your DOT to help build a Web3.0 Bridge Hub by joining our crowdloan, unlock RING and KTON bonus, also BTC and Metaverse limited edition commemorative NFT waiting for you.</p>
+          </div>        
+          <Link className={cx('banner-link')} to='plo'>
+            <span>Join our crowdloan</span>
+          </Link>
+        </div>
+      </Container>
+    </>
   );
 };
 
