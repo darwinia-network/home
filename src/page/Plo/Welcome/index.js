714eb042af5915b90d7ae7f8288029e65634918c
JayJay1024 <lygm1062@gmail.com>
2021-11-11 15:48:55 +0800

    chore: plo page learn more link

--- src/page/Plo/Welcome/index.js
+++ src/page/Plo/Welcome/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import styles from "./styles.module.scss";
 import classNames from "classnames/bind";
 import { Container } from "react-bootstrap";
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 
 import bridge from "./img/bridge.jpg";
 
@@ -15,12 +15,19 @@ const Welcome = () => {
         <img alt="..." src={bridge} className={cx("bridge")} />
         <h2 className={cx("title-h2")}>Support Darwinia, invest in the next generation of Web3.0 Metaverse</h2>
         <p className={cx("desc")}>Darwinia Network is bridging heterogeneous chains to build the Web3.0 Metaverse.</p>
-        {/* <a className={cx("sign-up-for-wautlist-link")} href="#waitlist-section">
-          <span>Sign up for waitlist</span>
-        </a> */}
-        <Link className={cx("sign-up-for-wautlist-link")} to='plo_contribute'>
-          <span>Join our crowdloan</span>
-        </Link>
+        <div className={cx("link-wrap")}>
+          <Link className={cx("join-our-crowdloan-link")} to="plo_contribute">
+            <span>Join our crowdloan</span>
+          </Link>
+          <a
+            className={cx("learn-more-link")}
+            href="https://darwinianetwork.medium.com/support-darwinia-while-investing-in-the-next-generation-infrastructure-for-the-web3-0-metaverse-aaf3defb234a"
+            rel="noopener noreferrer"
+            target="_blank"
+          >
+            <span>Lrean more</span>
+          </a>
+        </div>
       </div>
     </Container>
   );
