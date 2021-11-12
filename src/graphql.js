4274c5227bcf202034bf9e3707c2f50dd643c9dd
JayJay1024 <lygm1062@gmail.com>
2021-11-12 15:15:21 +0800

    chore: staging graphql

--- src/graphql.js
+++ src/graphql.js
@@ -4,6 +4,7 @@ export const graphqlClient = new ApolloClient({
   // uri: 'https://48p1r2roz4.sse.codesandbox.io',
   // uri: "https://api.subquery.network/sq/darwinia-network/kusama__ZGFyd",
   // uri: "https://api.subquery.network/sq/darwinia-network/polkadot__ZGFyd", // dev
-  uri: "https://api.subquery.network/sq/darwinia-network/polkadot", // prod
+  // uri: "https://api.subquery.network/sq/darwinia-network/polkadot", // prod
+  uri: "https://api.subquery.network/sq/darwinia-network/polkadot__ZGFyd", // staging
   cache: new InMemoryCache(),
 });
