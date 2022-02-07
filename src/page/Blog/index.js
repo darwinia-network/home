1531c92e7fa9a6cd4401322edf0d606d6e585a30
JayJay1024 <lygm1062@gmail.com>
2022-02-08 00:03:57 +0800

    chore: blog page header and footer

--- src/page/Blog/index.js
+++ src/page/Blog/index.js
@@ -1,7 +1,16 @@
 import React from "react";
+import styles from "./style.module.scss";
+import { PageHeader } from "../../components/PageHeader";
+import { PageFooter } from "../../components/PageFooter";
 
 const Blog = () => {
-  return <div>Blog Page</div>;
+  return (
+    <div>
+      <PageHeader forceFixed={true} />
+      <div className={styles.main}>Blog Page</div>
+      <PageFooter />
+    </div>
+  );
 };
 
 export default React.memo(Blog);
