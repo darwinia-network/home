4da219a86a35cbad448380040e99134f359a0136
JayJay1024 <lygm1062@gmail.com>
2022-02-08 11:35:11 +0800

    chore: blogs data

---
+++ src/page/Blog/data.js
@@ -0,0 +1,251 @@
+export const recentBlogs = [
+  {
+    picture: "./img/recent_blog_009.svg",
+    title: "Guide to Participate in the Crab PLO",
+    tag: ["Tutorials"],
+    abstract:
+      "We have structured a generous benefits package for our crowdloan contributors and sincerely hope you will consider loaning us your KSMs so that we can be successful in our bid for a parachain slot…",
+    time: "January 7, 2022",
+    link: "https://darwinianetwork.medium.com/guide-to-participate-in-the-crab-plo-e9b145bed4c9",
+  },
+  {
+    picture: "./img/recent_blog_008.svg",
+    title: "Technical Checklist for Developers Who Build on Crab Network",
+    tag: ["Tutorials"],
+    abstract:
+      "Crab Network will hold an online hackathon on the DoraHacks platform to fund the development of projects and applications that contribute to the Crab ecosystem and promote overall network growth…",
+    time: "January 7, 2022",
+    link: "https://darwinianetwork.medium.com/technical-checklist-for-developers-who-build-on-crab-network-73e79f5aec30",
+  },
+  {
+    picture: "./img/recent_blog_007.svg",
+    title: "Crab Online Hackathon Now Live!",
+    tag: ["Announcements"],
+    abstract:
+      "The Crab Hackathon aims to encourage developers to contribute ideas and early-stage projects related to Web3 Tools, DeFi, Metaverse, and NFT to the Crab ecosystem…",
+    time: "January 7, 2022",
+    link: "https://darwinianetwork.medium.com/crab-grant-online-hackathon-now-live-ea42a6c8cfbe",
+  },
+  {
+    picture: "./img/recent_blog_006.svg",
+    title: "Bifrost announced to integrate Darwinia Substrate-to-Substrate bridge for Defi applications",
+    tag: ["Announcements"],
+    abstract: "",
+    time: "",
+    link: "https://darwinianetwork.medium.com/bifrost-announced-to-integrate-darwinia-substrate-to-substrate-bridge-for-defi-applications-7215f5916521",
+  },
+  {
+    picture: "./img/recent_blog_005.svg",
+    title: "Support Crab While Investing in the Next Generation Infrastructure for the Web3.0 Metaverse",
+    tag: ["Blogs"],
+    abstract: "",
+    time: "",
+    link: "https://darwinianetwork.medium.com/support-crab-while-investing-in-the-next-generation-infrastructure-for-the-web3-0-metaverse-d194ff902f6d",
+  },
+  {
+    picture: "./img/recent_blog_004.svg",
+    title: "Crab Network To Join Kusama Parachain Auction",
+    tag: ["Announcements"],
+    abstract: "",
+    time: "",
+    link: "https://darwinianetwork.medium.com/crab-network-to-join-kusama-parachain-auction-db007bffb4d5",
+  },
+  {
+    picture: "./img/recent_blog_003.svg",
+    title: "BTC Rewards Release Now!",
+    tag: ["Announcements"],
+    abstract: "",
+    time: "",
+    link: "https://darwinianetwork.medium.com/btc-rewards-release-now-8c0ba0e571b5",
+  },
+  {
+    picture: "./img/recent_blog_002.svg",
+    title: "Polkadot Batch 2 Slot Auction Starts! Darwinia Crowdloan Pioneers BTC Rewards Release Soon!",
+    tag: ["Announcements"],
+    abstract: "",
+    time: "",
+    link: "https://darwinianetwork.medium.com/polkadot-batch-2-slot-auction-starts-darwinia-crowdloan-pioneers-btc-rewards-release-soon-bbdddff125dc",
+  },
+  {
+    picture: "./img/recent_blog_001.svg",
+    title: "Darwinia Bounty Program",
+    tag: ["Announcements"],
+    abstract: "",
+    time: "",
+    link: "https://darwinianetwork.medium.com/darwinia-bounty-program-e4e568d787dd",
+  },
+];
+
+export const tutorials = [
+  {
+    picture: "./img/tutorial_006.svg",
+    title: "Using Darwinia Tools 3–3: Smart App User Guide",
+    tag: ["Tutorials", "Tools"],
+    abstract:
+      "In a previous article, we have introduced a method to transfer tokens from the Substrate address to the Smart address; this article will introduce another method to achieve the same result by using Darwinia Smart App…",
+    time: "December 20, 2021",
+    link: "https://darwinianetwork.medium.com/using-darwinia-tools-3-3-smart-app-user-guide-c029db71102e",
+    subtype: "tools",
+  },
+  {
+    picture: "./img/tutorial_006.svg",
+    title:
+      "Using Darwinia Tools 3–2: Wormhole User Guide II | The Substrate-to-Substrate Bridge Between Darwinia Network and Crab Network",
+    tag: ["Tutorials", "Tools"],
+    abstract:
+      "We will demonstrate how to transfer assets between the Darwinia Network and Crab Network through the Wormhole…",
+    time: "December 20, 2021",
+    link: "https://darwinianetwork.medium.com/using-darwinia-tools-3-2-wormhole-user-guide-ii-the-substrate-to-substrate-bridge-between-443f21753c3",
+    subtype: "tools",
+  },
+  {
+    picture: "./img/tutorial_006.svg",
+    title: "Understanding Darwinia Bridge 1–6: The BSC Light Client for New Message Protocols",
+    tag: ["Tutorials", "Bridge"],
+    abstract:
+      "To accommodate the new design of the cross-chain message protocol, we need to update the light client accordingly. This article introduces the new design of the BSC light client on Darwinia…",
+    time: "December 16, 2021",
+    link: "https://darwinianetwork.medium.com/understanding-darwinia-bridge-1-6-the-bsc-light-client-for-new-message-protocols-33873f03273b",
+    subtype: "bridge",
+  },
+  {
+    picture: "./img/tutorial_005.svg",
+    title: "Understanding Darwinia Bridge 1–5: A Hands-on Guide on How to Run a Bridger Node",
+    tag: ["Tutorials", "Bridge"],
+    abstract:
+      "In a previous article, we have introduced our new design of a relayer fee market, which provides users with a new way of participating to earn in the Darwinia ecosystem…",
+    time: "December 13, 2021",
+    link: "https://darwinianetwork.medium.com/understanding-darwinia-bridge-1-5-a-hands-on-guide-on-how-to-run-a-bridger-node-55ba9ddd386d",
+    subtype: "bridge",
+  },
+  {
+    picture: "./img/tutorial_004.svg",
+    title: "Understanding Darwinia Bridge 1–4: The New Message Protocol for the Ethereum-Darwinia Bridge",
+    tag: ["Tutorials", "Bridge"],
+    abstract:
+      "In the layered structure of a cross-chain bridge, an ideal message protocol should be as consensus-agnostic and application-agnostic as possible…",
+    time: "December 13, 2021",
+    link: "https://darwinianetwork.medium.com/understanding-darwinia-bridge-1-4-the-new-message-protocol-for-the-ethereum-darwinia-bridge-6c4aeea5eab3",
+    subtype: "bridge",
+  },
+  {
+    picture: "./img/tutorial_004.svg",
+    title: "Using Darwinia Tools 3–1: Wormhole User Guide",
+    tag: ["Tutorials", "Tools"],
+    abstract:
+      "We will launch a series of articles to help users understand some of Darwinia’s tools, and this article is a user guide to the new version of Wormhole…",
+    time: "December 1, 2021",
+    link: "https://darwinianetwork.medium.com/understanding-darwinia-bridge-1-4-the-new-message-protocol-for-the-ethereum-darwinia-bridge-6c4aeea5eab3",
+    subtype: "tools",
+  },
+  {
+    picture: "./img/tutorial_003.svg",
+    title: "Build on Darwinia 2–1: Address Formats in Darwinia",
+    tag: ["Tutorials", "Builders"],
+    abstract:
+      "This article will introduce address formats we use in Darwinia, Crab and other test networks, and the relationship between them…",
+    time: "November 29, 2021",
+    link: "https://darwinianetwork.medium.com/build-on-darwinia-2-1-address-formats-in-darwinia-e964cc91fccc",
+    subtype: "builders",
+  },
+  {
+    picture: "./img/tutorial_002.svg",
+    title: "Understanding Darwinia Bridge 1–3: The Token Bridge Solution",
+    tag: ["Tutorials", "Bridge"],
+    abstract:
+      "The cross-chain token bridge refers to the asset transfer channel between two heterogeneous chains. Through the token bridge, assets can be safely and reliably transferred on different heterogeneous chains…",
+    time: "November 12, 2021",
+    link: "https://darwinianetwork.medium.com/understanding-darwinia-bridge-1-3-the-token-bridge-solution-1f178c9891aa",
+    subtype: "bridge",
+  },
+  {
+    picture: "./img/tutorial_001.svg",
+    title: "Understanding Darwinia Bridge 1–2: Mapping Token Factory",
+    tag: ["Tutorials", "Bridge"],
+    abstract:
+      "Every bridge has a sub-module called Mapping Token Factory in the Issuing module, responsible for creating, issuing, and burning mapping assets.…",
+    time: "October 29, 2021",
+    link: "https://darwinianetwork.medium.com/understanding-darwinia-bridge-1-2-mapping-token-factory-a77a2a2c47d1",
+    subtype: "bridge",
+  },
+  {
+    picture: "./img/tutorial_001.svg",
+    title: "Understanding Darwinia Bridge 1–1: Darwinia Relayer Incentive Scheme — Fee Market",
+    tag: ["Tutorials", "Bridge"],
+    abstract:
+      "Native tokens of the source chain are the only acceptable method of payment; The estimated fee of the message to be transferred is provided by a pricing system on the source chain which is connected to a Relayer Fee Market…",
+    time: "October 22, 2021",
+    link: "https://darwinianetwork.medium.com/understanding-darwinia-bridge-1-1-darwinia-relayer-incentive-scheme-fee-market-88308dab84a2",
+    subtype: "bridge",
+  },
+];
+
+export const tutorialsBridge = tutorials.find((v) => v.subtype === "bridge");
+export const tutorialsBuilders = tutorials.find((v) => v.subtype === "builders");
+export const tutorialsTools = tutorials.find((v) => v.subtype === "tools");
+
+export const newsLetters = [
+  {
+    picture: "./img/recent_newsletter_007.svg",
+    title: "Darwinia Monthly Dispatch: December 2021",
+    tag: ["Newsletters"],
+    abstract:
+      "Development Updates:Released Darwinia/Crab version 0.11.7, 0.11.8. Important notice to validators: v0.11.8 is a mandatory upgrade so please upgrade your nodes to this version ASAP before next runtime upgrade…",
+    time: "January 11, 2022",
+    link: "https://darwinianetwork.medium.com/darwinia-highlight-december-monthly-recap-c6600a0a1a0f",
+  },
+  {
+    picture: "./img/recent_newsletter_006.svg",
+    title: "Darwinia Monthly Dispatch: November 2021",
+    tag: ["Newsletters"],
+    abstract:
+      "Development Updates: Runtime upgrades for Pangolin and Pangoro test networks, spec versions are 2690, 26100, 27000. Refer https://github.com/darwinia-network/darwinia-common/releases …",
+    time: "December 3, 2021",
+    link: "https://darwinianetwork.medium.com/darwinia-highlight-november-monthly-recap-a933b6359ee4",
+  },
+  {
+    picture: "./img/recent_newsletter_005.svg",
+    title: "Darwinia Monthly Dispatch:October 2021",
+    tag: ["Newsletters"],
+    abstract:
+      "Development Updates: Technical preparation of Cumulus and Darwinia’s Parachain for upcoming Polkadot parachain auction PLO…",
+    time: "November 10, 2021",
+    link: "https://darwinianetwork.medium.com/darwinia-highlight-october-monthly-recap-695b10bccafc",
+  },
+  {
+    picture: "./img/recent_newsletter_004.svg",
+    title: "Darwinia Monthly Dispatch: September 2021",
+    tag: ["Newsletters"],
+    abstract:
+      "Development Updates: Deployed substrate to substrate bridge between Pangolin and Pangoro Test Network, and The first mapping token message has been successfully delivered through this bridge, more details…",
+    time: "October 11, 2021",
+    link: "https://darwinianetwork.medium.com/darwinia-highlight-september-monthly-recap-58c9e0324fa6",
+  },
+  {
+    picture: "./img/recent_newsletter_003.svg",
+    title: "Darwinia Monthly Dispatch: August 2021",
+    tag: ["Newsletters"],
+    abstract:
+      "Development Updates: Darwinia and Crab have released versions v0.11.2 and v0.11.3 and updated the Runtime via on-chain governance…",
+    time: "September 1, 2021",
+    link: "https://darwinianetwork.medium.com/darwinia-highlight-august-monthly-recap-dbfb97d1ca2d",
+  },
+  {
+    picture: "./img/recent_newsletter_002.svg",
+    title: "Darwinia Monthly Dispatch: July 2021",
+    tag: ["Newsletters"],
+    abstract:
+      "Development Updates: The Bridger reconstruction is completed, Darwinia-Ethereum, Pangolin-Millau have completed the new architecture migration, which is convenient for the subsequent development and maintenance of more bridges…",
+    time: "August 10, 2021",
+    link: "https://darwinianetwork.medium.com/darwinia-hight-july-monthly-recap-e9582d045af1",
+  },
+  {
+    picture: "./img/recent_newsletter_001.svg",
+    title: "Darwinia Monthly Dispatch: June 2021",
+    tag: ["Newsletters"],
+    abstract:
+      "Development Updates: Launch Kusama Parachain Slot Auction Crowdloan and participate in the auction; Add withdraw and KTON precompiled to DVM.…",
+    time: "July 2, 2021",
+    link: "https://darwinianetwork.medium.com/darwinia-hight-june-monthly-recap-c8c1a289c7d2",
+  },
+];
