ac1302ea09a92176078717537ae1c68369df7d46
JayJay1024 <lygm1062@gmail.com>
2022-03-14 22:28:56 +0800

    feat: remark plo nft address

--- src/page/PloContribute/components/metaverse-nft.js
+++ src/page/PloContribute/components/metaverse-nft.js
@@ -1,31 +1,116 @@
 import React, { useState } from "react";
 import classNames from "classnames/bind";
-import styles from "../styles.module.scss";
-import { Tooltip, Modal, Spin } from "antd";
+import { Tooltip, Modal, Spin, message } from "antd";
+import Web3Utils from "web3-utils";
+import { web3FromAddress } from "@polkadot/extension-dapp";
 
-import { DOT_TO_ORIG } from "../utils";
 import acceptIcon from "../img/accept.svg";
 import acceptedIcon from "../img/accepted.svg";
 import infoIcon from "../img/info-icon.png";
 import modalCloseIcon from "../img/modal-close.png";
 
+import { useApi } from "../hooks";
+import { DOT_TO_ORIG } from "../utils";
+import styles from "../styles.module.scss";
+
+import { ApolloClient, InMemoryCache, ApolloProvider, useQuery, gql } from "@apollo/client";
+
+const GET_MY_ADDRESS_REMARKS = gql`
+  query RemarkedNftAddresses($signer: String!) {
+    remarkedNftAddresses(
+      filter: { signer: { equalTo: $signer } }
+      orderBy: [BLOCK_NUMBER_ASC, EXTRINSIC_INDEX_ASC]
+      first: 5
+    ) {
+      nodes {
+        id
+        # signer
+        # value
+        # addressValue
+        # blockNumber
+        # extrinsicIndex
+        # extrinsicTimestamp
+        # extrinsicHash
+        # https://kusama.subscan.io/extrinsic/11416211-2
+        # https://kusama.subscan.io/extrinsic/0x59ffa39f7b6f08c0958f824b140a839e049dda47aa89ea6337257712d4d2e0bd
+      }
+    }
+  }
+`;
+
 const cx = classNames.bind(styles);
 
-const MetaverseNFT = ({ myTotalContribute }) => {
+const isAnValidDarwiniaAddress = (address) => {
+  console.log("address:", address);
+  return Web3Utils.isAddress(address || "");
+};
+
+const MetaverseNFT = ({ myTotalContribute, currentAccount }) => {
+  const { api } = useApi();
   const [isRemarked, setIsRemarked] = useState(false);
   const [isAccepted, setIsAccepted] = useState(false);
   const [claimLoading, setClaimLoading] = useState(false);
   const [visibleModalCopyThat, setVisibleModalCopyThat] = useState(false);
-  const [visibleModalClaimNFT, setVisibleModalClaimNFT] = useState(true);
+  const [visibleModalClaimNFT, setVisibleModalClaimNFT] = useState(false);
+  const [nftDarwiniaAddress, setNftDarwiniaAddress] = useState("");
 
-  // TODO:
-  void isRemarked;
-  void setIsRemarked;
-  void setClaimLoading;
+  const { loading, error, data } = useQuery(GET_MY_ADDRESS_REMARKS, {
+    variables: { signer: currentAccount ? currentAccount.address : "" },
+  });
+  error && console.error(error);
+  const myRemarked =
+    !loading && !error && data && data.remarkedNftAddresses && data.remarkedNftAddresses.nodes.length
+      ? data.remarkedNftAddresses.nodes[0]
+      : null;
 
-  const handleClickAcceptAndClaim = async () => {};
+  const handleClickAcceptAndClaim = async () => {
+    setClaimLoading(true);
+    const injector = await web3FromAddress(currentAccount.address);
+    api.setSigner(injector.signer);
 
-  const handleChangeNftCrabAddress = () => {};
+    try {
+      message.info("Please sign and send the transaction in Polkadot extension");
+
+      const mark = `NFT amount: 1; Rewards receiving address: ${nftDarwiniaAddress}`;
+      const unsubscribe = await api.tx.system.remark(mark).signAndSend(currentAccount.address, async (result) => {
+        if (!result || !result.status) {
+          return;
+        }
+        message.info("Extrinsic processing");
+
+        if (result.status.isFinalized || result.status.isInBlock) {
+          unsubscribe();
+
+          result.events
+            .filter(({ event: { section } }) => section === "system")
+            .forEach((res) => {
+              const {
+                event: { method },
+              } = res;
+
+              if (method === "ExtrinsicFailed") {
+                message.error("Extrinsic failed");
+              } else if (method === "ExtrinsicSuccess") {
+                message.success("Extrinsic success");
+                setIsRemarked(true);
+                setClaimLoading(false);
+                setVisibleModalClaimNFT(false);
+                setVisibleModalCopyThat(true);
+              }
+            });
+        }
+
+        if (result.isError) {
+          setClaimLoading(false);
+          message.error("Extrinsic Failed");
+        }
+      });
+    } catch (err) {
+      console.error(err);
+      setClaimLoading(false);
+      message.error("Extrinsic Failed");
+    }
+  };
 
   return (
     <>
@@ -62,9 +147,53 @@ const MetaverseNFT = ({ myTotalContribute }) => {
         <span className={cx("contribute-info-item-value")}>
           {myTotalContribute.gte(DOT_TO_ORIG.muln(10)) ? "1" : "0"}
         </span>
-        <button className={cx("claim-reward-btn")} disabled={true}>
-          <span>Claim</span>
-        </button>
+        {isRemarked || myRemarked ? (
+          <Tooltip
+            overlayClassName="tooltip-overlay"
+            overlayInnerStyle={{ padding: "20px", paddingBottom: "10px" }}
+            color="white"
+            placement="rightTop"
+            trigger={["click", "hover"]}
+            title={
+              <div>
+                <p className={cx("tips")}>
+                  <strong>Your address has been recorded!</strong>
+                </p>
+                <p className={cx("tips")}>
+                  NFT will be distributed after 2 rounds of Evolution Land Columbus Continent sales end.
+                </p>
+                <p className={cx("tips")}>
+                  Please track the NFT rewards by switching to{" "}
+                  <a target="_blank" rel="noopener noreferrer" href="https://www.evolution.land/lands">
+                    Columbus Continent
+                  </a>{" "}
+                  of Evolution Land, or stay tuned on our official Twitter.
+                </p>
+                <p className={cx("tips")}>
+                  Please feel free to contact us through "
+                  <a target="_blank" rel="noopener noreferrer" href="mailto:hello@darwinia.network">
+                    hello@darwinia.network
+                  </a>
+                  " if you have any questions.
+                </p>
+              </div>
+            }
+          >
+            <button className={cx("claim-reward-btn", "disabled")}>
+              <span className={cx("claim-reward-btn-text")}>Claimed</span>
+            </button>
+          </Tooltip>
+        ) : (
+          <button
+            className={cx("claim-reward-btn")}
+            disabled={myTotalContribute.lt(DOT_TO_ORIG.muln(10)) || loading || !currentAccount}
+            onClick={() => setVisibleModalClaimNFT(true)}
+          >
+            <Spin wrapperClassName={cx("metaverse-nft-modal-ok-btn-spin")} spinning={loading}>
+              <span className={cx("claim-reward-btn-text")}>Claim</span>
+            </Spin>
+          </button>
+        )}
       </div>
 
       <Modal
@@ -137,17 +266,27 @@ const MetaverseNFT = ({ myTotalContribute }) => {
           </div>
           <div className={cx("metaverse-nft-modal-address-input-wrap")}>
             <input
-              onChange={handleChangeNftCrabAddress}
+              onChange={(event) => setNftDarwiniaAddress(event.target.value)}
               className={cx("metaverse-nft-modal-address-input")}
-              disabled={true}
+              disabled={claimLoading || !isAccepted}
               placeholder="Please enter your receiving address"
             />
-            <span className={cx("metaverse-nft-modal-address-warning", { disbaled: false })}>
+            <span
+              className={cx("metaverse-nft-modal-address-warning", {
+                disabled: !nftDarwiniaAddress || isAnValidDarwiniaAddress(nftDarwiniaAddress),
+              })}
+            >
               Please fill in the correct receiving address starts with “0x”.{" "}
             </span>
           </div>
-          <button className={cx("metaverse-nft-modal-ok-btn")} disabled={true} onClick={handleClickAcceptAndClaim}>
-            <Spin wrapperClassName={cx("metaverse-nft-modal-ok-btn-spin")} spinning={claimLoading || true}>
+          <button
+            className={cx("metaverse-nft-modal-ok-btn")}
+            disabled={
+              claimLoading || !isAccepted || !nftDarwiniaAddress || !isAnValidDarwiniaAddress(nftDarwiniaAddress)
+            }
+            onClick={handleClickAcceptAndClaim}
+          >
+            <Spin wrapperClassName={cx("metaverse-nft-modal-ok-btn-spin")} spinning={claimLoading}>
               <span className={cx("metaverse-nft-modal-ok-btn-text")}>I accept and claim</span>
             </Spin>
           </button>
@@ -157,4 +296,15 @@ const MetaverseNFT = ({ myTotalContribute }) => {
   );
 };
 
-export default React.memo(MetaverseNFT);
+const client = new ApolloClient({
+  uri: "https://api.subquery.network/sq/darwinia-network/darwinia-nft-polkadot__ZGFyd",
+  cache: new InMemoryCache(),
+});
+
+const MetaverseNFTWrap = (props) => (
+  <ApolloProvider client={client}>
+    <MetaverseNFT {...props} />
+  </ApolloProvider>
+);
+
+export default React.memo(MetaverseNFTWrap);
