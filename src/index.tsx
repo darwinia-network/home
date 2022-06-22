cabfde596f9be0794cf74ab952061d408223a9eb
Ismail Mbarack <isunaslabs@gmail.com>
2022-06-22 16:55:01 +0800

    custom typewriter library completed

--- src/index.tsx
+++ src/index.tsx
@@ -8,9 +8,7 @@ import { HashRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
 root.render(
-  <React.StrictMode>
-    <HashRouter>
-      <App />
-    </HashRouter>
-  </React.StrictMode>
+  <HashRouter>
+    <App />
+  </HashRouter>
 );
--- src/pages/Demo/index.tsx
+++ src/pages/Demo/index.tsx
@@ -11,8 +11,33 @@ const Demo = () => {
     if (!element) {
       return;
     }
-    const typewriter = new Typewriter(element, { typingSpeed: 0, deletingSpeed: 0, loop: true });
-    console.log(typewriter);
+    const typewriter = new Typewriter(element, { typingSpeed: 100, deletingSpeed: 200, loop: true, cursor: "|" });
+    typewriter
+      .pauseFor(4000)
+      .typeString({
+        text: "Hello World",
+        style: "color: yellow; text-decoration: underline;",
+      })
+      .deleteAll(100)
+      .pauseFor(800)
+      .typeString(
+        {
+          text: "continue typing",
+          style: "color: blue;display: block;",
+        },
+        100
+      )
+      .deleteChars(4, 1000)
+      .typeString({
+        text: "after delete",
+        style: "color: pink;",
+      })
+      .pauseFor(4000)
+      .start();
+
+    return () => {
+      typewriter.clean();
+    };
   }, []);
 
   return (
