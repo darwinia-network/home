7e138a7f3bf31df6b87fdd2e1b35832646eedd40
JayJay1024 <lygm1062@gmail.com>
2023-09-11 14:12:24 +0800

    remove js-sdk-api

--- src/locale/localeKeys.ts
+++ src/locale/localeKeys.ts
@@ -162,7 +162,6 @@ const localeKeys = {
   createASimpleDapp: "createASimpleDapp",
   ethereumDarwiniaParachain: "ethereumDarwiniaParachain",
   soliditySDKApi: "soliditySDKApi",
-  jsSDKApi: "jsSDKApi",
   solutions: "solutions",
   darwiniaChain: "darwiniaChain",
   darwiniaMsgport: "darwiniaMsgport",
