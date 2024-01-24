30384a148b73a71f1053b424b3d435b98499c4e3
JayJay1024 <lygm1062@gmail.com>
2024-01-24 17:49:27 +0800

    Copyright component

---
+++ src/components/Copyright.tsx
@@ -0,0 +1,7 @@
+export default function Copyright() {
+  return (
+    <span className="text-t14 text-t16 text-white lg:text-app-white">
+      © {new Date().getFullYear()} Darwinia Network
+    </span>
+  );
+}
