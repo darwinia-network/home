1cc4a6b88577b4a8dde810a9a766d6fad19b7a3f
JayJay1024 <lygm1062@gmail.com>
2024-01-24 15:55:14 +0800

    PoweringYourDapps component

---
+++ src/components/PoweringYourDapps.tsx
@@ -0,0 +1,19 @@
+export default function PoweringYourDapps() {
+  return (
+    <div className="flex flex-col">
+      <span className="text-2xl font-medium tracking-[1px] lg:text-6xl lg:font-normal text-black">{`Powering Your DApp's`}</span>
+      <div className="mt-[7px] lg:mt-3 flex flex-col text-app-main uppercase text-h1 lg:font-bold lg:tracking-[1px] lg:leading-[122px] lg:text-[120px]">
+        <span>Cross-Chain</span>
+        <span>Capabilities</span>
+      </div>
+      <button className="mt-[22px] lg:mt-[30px] flex items-center gap-medium py-[9px] px-[10px] lg:py-5 lg:px-8 rounded-[100px] bg-black w-fit">
+        <span className="text-t14 lg:text-t20 text-white">Develop with Darwinia</span>
+        <img
+          className="w-[16px] h-[16px] lg:w-[24px] lg:h-[24px]"
+          alt="..."
+          src="/images/right-arrow-white-background-round.svg"
+        />
+      </button>
+    </div>
+  );
+}
