024637ab556fa667c077c5f51ee22a0ed6fb92f2
JayJay1024 <lygm1062@gmail.com>
2024-01-29 04:11:56 +0800

    Projects

--- src/pages/Home2/sections/ResourcesSection.tsx
+++ src/pages/Home2/sections/ResourcesSection.tsx
@@ -1,19 +1,29 @@
+import Projects from "../../../components/Projects";
 import ResourcesV2 from "../../../components/ResourcesV2";
 
 export function ResourcesSectionMobile() {
   return (
-    <div className="bg-app-white px-[1.25rem] py-[6.25rem]">
-      <ResourcesV2 />
+    <div className="bg-app-white pb-[6.25rem]">
+      <Projects />
+      <div className="h-[6.25rem]" />
+      <div className="px-[1.25rem]">
+        <ResourcesV2 />
+      </div>
     </div>
   );
 }
 
 export function ResourcesSectionPC() {
   return (
-    <div className="bg-app-white px-[7.375rem] h-[62.5rem] flex flex-col justify-center relative">
+    <div className="bg-app-white px-[7.5rem] h-[62.5rem] flex flex-col justify-center relative">
       <div className="w-[20rem] h-[20rem] shrink-0 bg-app-main opacity-20 rounded-full blur-[200px] absolute z-10 -top-[11rem] right-[10rem]" />
       <div className="w-[20rem] h-[20rem] shrink-0 bg-app-main opacity-20 rounded-full blur-[200px] absolute z-10 -bottom-[14rem] left-[6rem]" />
-      <ResourcesV2 />
+      <div className="absolute z-10 top-0">
+        <Projects isVertical />
+      </div>
+      <div className="pl-[7.5rem]">
+        <ResourcesV2 />
+      </div>
     </div>
   );
 }
