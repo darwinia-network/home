b31f30b967442dcc90743eb1e3fc6c45a7a2e77a
JayJay1024 <lygm1062@gmail.com>
2024-01-29 12:04:27 +0800

    Test scroll

--- src/pages/Home2/index.tsx
+++ src/pages/Home2/index.tsx
@@ -4,8 +4,16 @@ import HomeV2PC from "./pc";
 export default function HomeV2() {
   return (
     <>
-      <HomeV2Mobile />
-      <HomeV2PC />
+      {false && <HomeV2Mobile />}
+      {false && <HomeV2PC />}
+      <main className="overflow-auto lg:flex items-center hidden scrollbar-color">
+        <div className="bg-app-main h-[120vh] w-[160vw] relative shrink-0">
+          <div className="absolute z-10 text-black top-0 left-0">Top Left</div>
+          <div className="absolute z-10 text-black top-0 right-0">Top Right</div>
+          <div className="absolute z-10 text-black bottom-0 left-0">Bottom Left</div>
+          <div className="absolute z-10 text-black bottom-0 right-0">Bottom Right</div>
+        </div>
+      </main>
     </>
   );
 }
