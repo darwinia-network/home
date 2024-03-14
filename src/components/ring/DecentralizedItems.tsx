86a0ef456c9215b315e933e4d8c9a0cf20286945
Hamid Roohi <52012062+hamidroohi71@users.noreply.github.com>
2024-03-14 12:27:30 +0330

    fix pull request comment

--- src/components/ring/DecentralizedItems.tsx
+++ src/components/ring/DecentralizedItems.tsx
@@ -6,22 +6,19 @@ interface DecentralizedType {
 
 const DecentralizedItems = ({item} : DecentralizedType) => {
   return (
-    <div className='flex items-center gap-[10px] lg:gap-0 lg:flex-col w-full bg-[#2A2A2A] lg:bg-transparent p-[10px] lg:p-0 rounded-[10px] lg:w-[104px]'>
+    <div className='flex items-center gap-[10px] lg:gap-0 lg:flex-col w-full bg-[#2A2A2A] lg:bg-transparent p-[10px] lg:p-0 rounded-[10px] lg:w-[104px] cursor-pointer' onClick={() => {
+      window.open(item.link, '_blank');
+  }}>
         <img src={item.image} alt={item.text} className='w-[40px] h-[40px] lg:w-[70px] lg:h-[70px]'/>
         <div className='flex items-center gap-[5px]'>
             <p className='text-[14px] font-[700] leading-[22.4px] tracking-[1px] text-[#F2F3F5] flex-shrink-0'>{item.text}</p>
-            <img src="/images/ring/UnionBridge.svg" alt="union" className='hidden lg:block cursor-pointer' onClick={() => {
-                  window.open(item.link, '_blank');
-
-            }}/>
+            <img src="/images/ring/UnionBridge.svg" alt="union" className='hidden lg:block cursor-pointer' />
         </div>
         {
             item.subtext &&
             <p className='text-[12px] font-[300] leading-[15.22px] text-[#F2F3F5] flex-shrink-0 opacity-50 w-[106px] lg:w-auto'>{item.subtext}</p>
           }
-          <img src="/images/ring/UnionBridge.svg" alt="union" className='lg:hidden cursor-pointer'  onClick={() => {
-                window.open(item.link, '_blank');
-          }}/>
+          <img src="/images/ring/UnionBridge.svg" alt="union" className='lg:hidden cursor-pointer'/>
     </div>
   )
 }
