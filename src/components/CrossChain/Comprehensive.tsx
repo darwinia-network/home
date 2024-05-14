6619ba27a40367ff649efd190151df4883f827a6
Hamid Roohi <hamidroohi71@gmail.com>
2024-05-14 12:02:02 -0400

    Add links to buttons

--- src/components/CrossChain/Comprehensive.tsx
+++ src/components/CrossChain/Comprehensive.tsx
@@ -11,14 +11,19 @@ const Comprehensive = () => {
       />
       <div className="flex flex-col items-center justify-center lg:block">
         <TitleAndTextSection
-          text="Msgport is a programmable Cross-Chain Messaging Port. Just as smart contracts provide programmability for DApp developers, integration with Msgport will empower developers on Darwinia Chain with the capability for cross-chain programming.To develop more interesting cross-chain DApps on Darwinia Chain using Msgport."
+          text="Msgport is a programmable Cross-Chain Messaging Port. Just as smart contracts provide programmability for DApp developers, integration with Msgport will empower developers on Darwinia Chain with the capability for cross-chain programming."
           title="Comprehensive Cross-Chain Stack"
           classes="lg:w-[21.5rem] 2xl:w-[34.375rem]"
         />
-        <button className="flex items-center gap-[0.625rem] text-[0.875rem] text-[#F6F6F7] leading-[1.375rem] tracking-[0.063rem] py-[0.625rem] px-[0.938rem] bg-black z-10 rounded-[6.25rem] mt-[1.25rem] font-[700] transition-all delay-75 hover:bg-[#FF0083]">
-          learn more
+        <TitleAndTextSection
+          text="To develop more interesting cross-chain DApps on Darwinia Chain using Msgport."
+          title=""
+          classes="lg:w-[21.5rem] 2xl:w-[34.375rem]"
+        />
+        <a href="https://msgport.xyz" target="blank" className="flex w-fit items-center gap-[0.625rem] text-[0.875rem] text-[#F6F6F7] leading-[1.375rem] tracking-[0.063rem] py-[0.625rem] px-[0.938rem] bg-black z-10 rounded-[6.25rem] mt-[1.25rem] font-[700] transition-all delay-75 hover:bg-[#FF0083]">
+          Learn more
           <img src="/images/right-arrow-white-background-round.svg" alt="right-arrow" />
-        </button>
+        </a>
       </div>
     </section>
   );
