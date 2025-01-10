dd983cd889078a406b23cb5ab8e2aa91b6ec173c
Bear Wang <boundless.forest@outlook.com>
2025-01-10 10:26:24 +0800

    Update learn more (#445)
    * Update learn more
    
    * Use npm
    
    ---------
    
    Co-authored-by: fewensa <37804932+fewensa@users.noreply.github.com>

--- src/components/CrossChain/GovernedSection.tsx
+++ src/components/CrossChain/GovernedSection.tsx
@@ -5,11 +5,11 @@ const GovernedSection = () => {
         Governed by
       </h3>
       <img src="/images/crossChain/ringDa.png" alt="ringDa" className="w-[16.25rem] lg:w-[25rem] 2xl:w-auto" />
-      <p className="text-[0.875rem] text-[#F6F6F7] leading-[1.4rem] tracking-[0.063rem] font-[400] mt-[1.25rem] text-center lg:w-[49rem] ">
+      <p className="text-[0.875rem] text-[#F6F6F7] leading-[1.4rem] tracking-[0.063rem] font-[400] mt-[1.25rem] text-center lg:w-[49rem]">
         <a href="https://ringdao.com/">RingDAO</a> refers to the next governance version of Darwinia. RING serves as the governance token for RingDAO, and
         the upgrades to Darwinia Chain will be governed by RingDAO. RING also functions as the Gas Token for Darwinia
         Chain and plays a role in Collator Staking, among other aspects. RingDAO also governs other projects, including
-        Msgport and XAPI. <a href="https://ringdao.com/">Learn more</a>.
+        Msgport and XAPI. For details, visit <a href="https://ringdao.com/">RingDAO’s website</a>.
       </p>
     </section>
   );
